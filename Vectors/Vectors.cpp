4e73f5afd26194b7cb0ce0cce92df841f1684904
fzyrkowski <fzyrkowski@WRL-00L123-L014>
2024-04-18 16:16:57 -0500

    soccer players + cups + average display with vectors

--- Vectors/Vectors.cpp
+++ Vectors/Vectors.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
 using namespace std;
 int main()
 {
 	int n;
-	vector<int> temp;
-	vector<vector<int>> num;
-	//No fixed size when it comes to a vector within a vector
-	//temp acts as a temporary vector to fill the num vector
-	//NEED TO CONTROL THE SIZE OF THE VECTOR
+	string name;
+	int cup;
+	double average;
+	vector<string> players;
+	vector<double> cups;
+	vector<double> avg;
+	vector<vector<double>> table;
+	//10 rows, 7 columns
 
-	for (int r =0; r < 3; r++)
+	for (int r = 0; r < 10; r++) 
 	{
-		for (int c = 0; c < 4; c++)
+		for (int c = 0; c < 7; c++)
 		{
-			cout << "Enter row # " << (r + 1) << "Column # " << (c + 1) << " ==>";
-			cin >> n;
-			temp.push_back(n);
+			cin.ignore();
+			if(c == 0){
+				cout << "Enter player # " << (r + 1) << ":";
+				getline(cin, name);
+				players.push_back(name);
+			}
+			else if(c > 0 && c != 6)
+			{
+				cout << "Enter cup # " << c << ":";
+				cin >> cup;
+				cups.push_back(cup);
+			}
+			table.push_back(cups);
+			cups.clear();
 		}
-		num.push_back(temp);
-		temp.clear();
-		//erases all the current values from temp
 	}
 
-	cout << "\n==================================\n\n";
-
-	for (int r = 0; r < num.size(); r++)
+	for (int r = 0; r < table.size(); r++)
 	{
-		for (int c = 0; c < num[r].size(); c++)
-		//size of the vector for row r
+		cout << players[r][0] << "\t\t";
+		for (int c = 0; c < table[r].size(); c++)
 		{
-			cout << num[r][c] << "\t";
+			cout << table[r][c] << "\t";
 		}
-		cout << endl;
 	}
 }
